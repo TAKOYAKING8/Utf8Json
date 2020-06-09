21ebb6cf3eb5824fd10f2f6627910a0f6ba2154a
TAKOYAKING <takoyaking88@gmail.com>
2020-06-09 17:42:13 +0900

    down

--- src/Utf8Json.UniversalCodeGenerator/CodeAnalysis/TypeCollector.cs
+++ src/Utf8Json.UniversalCodeGenerator/CodeAnalysis/TypeCollector.cs
@@ -401,7 +401,7 @@ namespace Utf8Json.UniversalCodeGenerator
                     IsProperty = true,
                     IsField = false,
                     Name = name,
-                    Namespace = type.ContainingNamespace.ToDisplayString(),
+                    Namespace = item.ContainingNamespace.ToDisplayString(),
                     Type = item.Type.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat),
                     ShortTypeName = item.Type.ToDisplayString(binaryWriteFormat)
                 };
@@ -427,7 +427,7 @@ namespace Utf8Json.UniversalCodeGenerator
                     IsProperty = false,
                     IsField = true,
                     Name = name,
-                    Namespace = type.ContainingNamespace.ToDisplayString(),
+                    Namespace = item.ContainingNamespace.ToDisplayString(),
                     Type = item.Type.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat),
                     ShortTypeName = item.Type.ToDisplayString(binaryWriteFormat)
                 };
