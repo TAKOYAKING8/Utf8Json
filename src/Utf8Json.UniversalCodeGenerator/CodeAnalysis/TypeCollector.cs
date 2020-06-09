a312fbce6f341b5a6a7fc46540864947f0fefc6d
TAKOYAKING <takoyaking88@gmail.com>
2020-06-10 00:03:11 +0900

    down

--- src/Utf8Json.UniversalCodeGenerator/CodeAnalysis/TypeCollector.cs
+++ src/Utf8Json.UniversalCodeGenerator/CodeAnalysis/TypeCollector.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Semantics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
